f68caf788b7f390524bdd36981f093e67b0a286b
Felix Dominguez <felixomardominguez847@gmail.com>
2024-06-05 12:43:38 -0600

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import subprocess
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,19 +10,37 @@ CORS(app)
 def run_code():
     data = request.get_json()
     code = data['code']
+    file_extension = data.get('file_extension', 'py')
     
+    if file_extension == 'java':
+        return run_java_code(code)
+    elif file_extension == 'py':
+        return run_python_code(code)
+    else:
+        return jsonify({'output': 'Unsupported file extension'})
+
+def run_java_code(code):
     try:
-        result = subprocess.run(
-            ['python', '-c', code],
-            capture_output=True,
-            text=True,
-            check=True
-        )
-        output = result.stdout
+        with open('Main.java', 'w') as java_file:
+            java_file.write(code)
+        result = subprocess.run(['javac', 'Main.java'], capture_output=True, text=True, check=True)
+        if result.returncode != 0:
+            return jsonify({'output': result.stderr})
+        result = subprocess.run(['java', 'Main'], capture_output=True, text=True, check=True)
+        return jsonify({'output': result.stdout})
     except subprocess.CalledProcessError as e:
-        output = e.stderr
-    
-    return jsonify({'output': output})
+        return jsonify({'output': e.stderr})
+    finally:
+        os.remove('Main.java')
+        if os.path.exists('Main.class'):
+            os.remove('Main.class')
+
+def run_python_code(code):
+    try:
+        result = subprocess.run(['python', '-c', code], capture_output=True, text=True, check=True)
+        return jsonify({'output': result.stdout})
+    except subprocess.CalledProcessError as e:
+        return jsonify({'output': e.stderr})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
